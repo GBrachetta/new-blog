b0dc8b48497995b97ef07eea9e54421c6c574ea1
Guillermo Brachetta <brachetta@me.com>
2020-11-26 22:55:47 +0100

    Added auth views and templates

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -11,9 +12,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Accout for {username} created")
-            return redirect("blog-home")
+            messages.success(
+                request, f"Accout for {username} created. You can now login"
+            )
+            return redirect("login")
 
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    """Profile"""
+    return render(request, "users/profile.html")
