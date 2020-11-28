dbd6215acc38d815ba826eeed94f599f8ae6af6a
Guillermo Brachetta <brachetta@me.com>
2020-11-29 00:35:07 +0100

    edit photo in edit post feature

--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(
         default="default.jpg",
-        upload_to="profile_pics",
+        upload_to="profile_pics/",
         validators=[FileExtensionValidator(["jpeg", "jpg", "png"])],
     )
 
