03441a541b2b4aba8337755e934a1afe378c6682
Guillermo Brachetta <brachetta@me.com>
2020-11-29 00:19:03 +0100

    Create thumbnail for profile image

--- users/models.py
+++ users/models.py
@@ -1,20 +1,23 @@
 from django.contrib.auth.models import User
 from django.db import models
-from PIL import Image
+from blog.make_thumbnail import make_thumbnail
+from django.core.validators import FileExtensionValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+    image = models.ImageField(
+        default="default.jpg",
+        upload_to="profile_pics",
+        validators=[FileExtensionValidator(["jpeg", "jpg", "png"])],
+    )
 
     def __str__(self):
         return f"{self.user.username} Profile"
 
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
-
-    #     img = Image.open(self.image.path)
-    #     if img.height > 300 or img.width > 300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
+    def save(self, *args, **kwargs):
+        if self.image:
+            self.image = make_thumbnail(self.image, size=(200, 200))
+            super().save(*args, **kwargs)
+        else:
+            super().save(*args, **kwargs)
