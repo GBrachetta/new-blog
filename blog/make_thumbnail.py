270ee2cacb5a0e3ff53c9eda560562cf890f02d0
Guillermo Brachetta <brachetta@me.com>
2020-11-30 15:56:31 +0100

    Extend rotate method to post pictures

--- blog/make_thumbnail.py
+++ blog/make_thumbnail.py
@@ -3,6 +3,31 @@ from django.core.files import File
 from PIL import Image
 
 
+# def make_thumbnail(image, size=(600, 600)):
+#     """
+#     Uses PIL to generate thumbnail.
+#     Checks for file type and converts it accordingly.
+#     The image gallery can then use a lightweight thumbnail to
+#     display the images and renders the full size image only when the
+#     user clicks on the thumbnail, reducing loading time dramatically.
+#     """
+
+#     im = Image.open(image)
+#     if im.format == "JPEG":
+#         im.convert("RGB")
+#         im.thumbnail(size)
+#         thumb_io = BytesIO()
+#         im.save(thumb_io, "JPEG", quality=85)
+#         image = File(thumb_io, name=image.name)
+#     else:
+#         im.convert("RGBA")
+#         im.thumbnail(size)
+#         thumb_io = BytesIO()
+#         im.save(thumb_io, "PNG", quality=85)
+#         image = File(thumb_io, name=image.name)
+#     return image
+
+
 def make_thumbnail(image, size=(600, 600)):
     """
     Uses PIL to generate thumbnail.
@@ -12,17 +37,30 @@ def make_thumbnail(image, size=(600, 600)):
     user clicks on the thumbnail, reducing loading time dramatically.
     """
 
-    im = Image.open(image)
-    if im.format == "JPEG":
-        im.convert("RGB")
-        im.thumbnail(size)
-        thumb_io = BytesIO()
-        im.save(thumb_io, "JPEG", quality=85)
-        image = File(thumb_io, name=image.name)
-    else:
-        im.convert("RGBA")
-        im.thumbnail(size)
-        thumb_io = BytesIO()
-        im.save(thumb_io, "PNG", quality=85)
-        image = File(thumb_io, name=image.name)
-    return image
+    with Image.open(image) as im:
+        if im.format == "JPEG":
+            exif = im._getexif()
+
+            im.convert("RGB")
+            im.thumbnail(size)
+            thumb_io = BytesIO()
+            orientation_key = 274
+            if exif and orientation_key in exif:
+                orientation = exif[orientation_key]
+                rotate_values = {
+                    3: Image.ROTATE_180,
+                    6: Image.ROTATE_270,
+                    8: Image.ROTATE_90,
+                }
+
+                if orientation in rotate_values:
+                    im = im.transpose(rotate_values[orientation])
+            im.save(thumb_io, "JPEG", quality=85)
+            image = File(thumb_io, name=image.name)
+        else:
+            im.convert("RGBA")
+            im.thumbnail(size)
+            thumb_io = BytesIO()
+            im.save(thumb_io, "PNG", quality=85)
+            image = File(thumb_io, name=image.name)
+        return image
--- users/models.py
+++ users/models.py
@@ -2,9 +2,6 @@ from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
 from django.db import models
 from blog.make_thumbnail import make_thumbnail
-from PIL import Image
-from io import BytesIO
-from django.core.files import File
 
 
 class Profile(models.Model):
@@ -22,44 +19,5 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         if self.image and not self.is_resized:
             self.is_resized = True
-            # self.image = make_thumbnail(self.image, size=(200, 200))
-            self.image = make_new_thumbnail(self.image, size=(200, 200))
+            self.image = make_thumbnail(self.image, size=(200, 200))
         super().save(*args, **kwargs)
-
-
-def make_new_thumbnail(image, size=(600, 600)):
-    """
-    Uses PIL to generate thumbnail.
-    Checks for file type and converts it accordingly.
-    The image gallery can then use a lightweight thumbnail to
-    display the images and renders the full size image only when the
-    user clicks on the thumbnail, reducing loading time dramatically.
-    """
-
-    with Image.open(image) as im:
-        if im.format == "JPEG":
-            exif = im._getexif()
-
-            im.convert("RGB")
-            im.thumbnail(size)
-            thumb_io = BytesIO()
-            orientation_key = 274
-            if exif and orientation_key in exif:
-                orientation = exif[orientation_key]
-                rotate_values = {
-                    3: Image.ROTATE_180,
-                    6: Image.ROTATE_270,
-                    8: Image.ROTATE_90,
-                }
-
-                if orientation in rotate_values:
-                    im = im.transpose(rotate_values[orientation])
-            im.save(thumb_io, "JPEG", quality=85)
-            image = File(thumb_io, name=image.name)
-        else:
-            im.convert("RGBA")
-            im.thumbnail(size)
-            thumb_io = BytesIO()
-            im.save(thumb_io, "PNG", quality=85)
-            image = File(thumb_io, name=image.name)
-        return image
